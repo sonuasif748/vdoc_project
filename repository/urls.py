3d36b7672003d78ccc504dbe925cf2a0bc12f978
sonuasif748 <sonuasif748@gmail.com>
2021-10-06 14:14:08 +0530

    'test3'

--- repository/urls.py
+++ repository/urls.py
@@ -23,5 +23,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('projects.urls')),
     path('', include('user.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),\
-              static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
